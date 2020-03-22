90c5b5f3942e6cd7445462273872d80f66c9b412
EliardoItla <60952674+EliardoItla@users.noreply.github.com>
2020-03-22 12:36:16 -0400

    Add files via upload

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Singleton.Instance.msj);
+            Singleton.Instance.msj = "Tipico Gurabo";
+            Console.WriteLine(Singleton.Instance.msj);
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+
+        public string msj = "";
+
+        protected Singleton() {
+            msj = "El Tipico Hola Mundo xD";
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+
+                return instance;
+            }
+        }
+    }
+}
